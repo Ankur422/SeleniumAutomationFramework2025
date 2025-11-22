3cc490e61f8a7dfa21d4a250b6fd6dd391d5ced9
unknown <aman.subhi95@gmail.com>
2025-11-22 21:33:11 +0530

    extend report

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,14 +2,35 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITest;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 	
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setupreport() {
+		
+		extent =ExtentReportManager.getReportInstance();
+	}
+	@AfterSuite
+	public void teardonReport() {
+		extent.flush();
+	}
 	
 	@BeforeMethod
 	public void setup() {
@@ -22,7 +43,15 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+		if(result.getStatus()==ITestResult.FAILURE) {
+			String screenshotPath=ExtentReportManager.captureScreenshot(driver,"LoginFailure");
+			System.out.println("Screenshot Captures, PATH:"+screenshotPath);
+			test.fail("Test Failed. Screenshot attached" ,MediaEntityBuilder.createScreenCaptureFromBase64String(screenshotPath).build());
+		}
+		
+		
 		
 		if(driver!=null) {
 			
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
@@ -15,16 +16,42 @@ public class LoginTest extends BaseTest {
 	public void testvalidLogin() {
 		
 		Log.info("Starting login test...");
+		test =ExtentReportManager.createTest("Login Test");
 		LoginPage loginPage =new LoginPage(driver);
 		
+		test.info("Navigating to URL");
 		loginPage.enterusername("admin@yourstore.com");
 		loginPage.enterpassword("admin");
 		loginPage.submit();
 		
 	String title=	driver.getTitle();
 	System.out.println(title);
+	
+	
 	Log.info("Verfying ...");
 	Assert.assertEquals(driver.getTitle(), "Just a moment...");
+		
+		
+	}
+	
+@Test	
+public void testLoginwithIvalidCredentials() {
+		
+		Log.info("Starting login test...");
+		test =ExtentReportManager.createTest("Login Test with Invalid credentials");
+		LoginPage loginPage =new LoginPage(driver);
+		
+		test.info("Navigating to URL");
+		loginPage.enterusername("admin23@yourstore.com");
+		loginPage.enterpassword("admin778");
+		loginPage.submit();
+		
+	String title=	driver.getTitle();
+	System.out.println(title);
+	
+	
+	Log.info("Verfying ...");
+	Assert.assertEquals(driver.getTitle(), "Just a moment...789");
 		
 		
 	}
